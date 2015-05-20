3af7b37c5d8e984dc3a528163f44cba1a8c3f076
Rui Yang <ryang.sei@gmail.com>
2015-05-20 13:57:43 +1000

    remove electric-pair-mode, prefer smartparens

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -1,10 +1,5 @@
 (require-package 'unfill)
 
-(when (fboundp 'electric-pair-mode)
-  (electric-pair-mode))
-(when (eval-when-compile (version< "24.4" emacs-version))
-  (electric-indent-mode 1))
-
 ;;----------------------------------------------------------------------------
 ;; Some basic preferences
 ;;----------------------------------------------------------------------------
