dd6deaeb4e9ffa3c34bf65ea1f81913591acde1d
Rui Yang <ryang.sei@gmail.com>
2015-06-17 22:54:12 +1000

    use web-mode for template editing

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -2,11 +2,17 @@
 
 (require-package 'tagedit)
 (require-package 'web-mode)
-(after-load 'sgml-mode
+(after-load 'web-mode
   (tagedit-add-paredit-like-keybindings)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
+  (add-hook 'web-mode-hook (lambda () (tagedit-mode 1))))
 
-(add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
 ;; Note: ERB is configured in init-ruby-mode
 
