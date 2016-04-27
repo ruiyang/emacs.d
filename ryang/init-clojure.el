bb0308b2c425de0b78a657dd6049ad3e8c7daa3e
Rui Yang <ryang.sei@gmail.com>
2016-04-27 22:46:01 +1000

    init cljs repl options in init-clojure

--- ryang/init-clojure.el
+++ ryang/init-clojure.el
@@ -12,4 +12,10 @@
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
+(require 'cider)
+(setq cider-cljs-lein-repl
+      "(do (require 'figwheel-sidecar.repl-api)
+           (figwheel-sidecar.repl-api/start-figwheel!)
+           (figwheel-sidecar.repl-api/cljs-repl))")
+
 (provide 'init-clojure)
