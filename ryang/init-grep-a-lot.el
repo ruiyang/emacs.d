c32189b0e8b1fd665578219ce2caa1a736f23847
Rui Yang <ryang.sei@gmail.com>
2016-11-07 15:51:11 +1100

    add and config grep a lot

--- ryang/init-grep-a-lot.el
+++ ryang/init-grep-a-lot.el
@@ -1,8 +1,8 @@
-(require-package 'grep-a-lot)
+;;(require-package 'grep-a-lot)
 (require 'grep-a-lot)
 (grep-a-lot-setup-keys)
 
 ;; customize find-grep
 (setq grep-find-command "find . -name 'target' -prune -o -name 'webapp*assets' -prune -o -name '.bundle' -prune -o -path '*/tmp/*' -prune -o -name 'public' -prune -o -name 'cache' -prune -o -name '*' ! -name '*~' ! -name 'old-*.js' ! -name 'old-*.css' ! -name 'ext*.js' ! -name 'yui*.js' ! -name '*.dll' ! -name '*.pdb' ! -name 'development.log' -type f -print0 | xargs -0 grep -H -n ")
 
-(provide 'setup-grep)
+(provide 'init-grep-a-lot)
